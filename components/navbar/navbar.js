d124255186ad999da8f44e39be23cd7829b2214f
Won Lee <whd793@gmail.com>
2022-08-23 02:08:27 +0900

    added static banner, navigation bar, and some stylings

---
+++ components/navbar/navbar.js
@@ -0,0 +1,34 @@
+import styles from "./navbar.module.css";
+
+const Navbar = (props) => {
+  const { username } = props;
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink}>
+          <div className={styles.logoWrapper}>Netflix</div>
+        </a>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem}>Home</li>
+          <li className={styles.navItem2}>My List</li>
+        </ul>
+        <nav className={styles.navContainer}>
+          <div>
+            <button className={styles.usernameBtn}>
+              <p className={styles.username}>{username}</p>
+            </button>
+
+            <div className={styles.navDropdown}>
+              <div>
+                <a className={styles.linkName}>Sign out</a>
+                <div className={styles.lineWrapper}></div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
