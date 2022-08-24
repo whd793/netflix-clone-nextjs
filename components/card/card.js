ee442ebbe4502b9806dd60638936cb886b80e6da
Won Lee <whd793@gmail.com>
2022-08-24 16:26:33 +0900

    added card component, framer motion, and youtube api data

--- components/card/card.js
+++ components/card/card.js
@@ -1,10 +1,48 @@
+import { useState } from "react";
 import Image from "next/image";
+import styles from "./card.module.css";
+import { motion } from "framer-motion";
+
+import cls from "classnames";
 
 const Card = (props) => {
-  const { imgUrl, size } = props;
+  const {
+    imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80",
+    size = "medium",
+    id,
+  } = props;
+
+  const [imgSrc, setImgSrc] = useState(imgUrl);
+  const classMap = {
+    large: styles.lgItem,
+    medium: styles.mdItem,
+    small: styles.smItem,
+  };
+
+  const handleOnError = () => {
+    console.log("hii error");
+    setImgSrc(
+      "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80"
+    );
+  };
+
+  const scale = id === 0 ? { scaleY: 1.1 } : { scale: 1.1 };
+
   return (
-    <div>
-      <Image src={imgUrl} alt="image" width="300px" height="300px" />
+    <div className={styles.container}>
+      {/* Card */}
+      <motion.div
+        className={cls(styles.imgMotionWrapper, classMap[size])}
+        whileHover={{ ...scale }}
+      >
+        <Image
+          src={imgSrc}
+          alt="image"
+          layout="fill"
+          onError={handleOnError}
+          className={styles.cardImg}
+        />
+      </motion.div>
     </div>
   );
 };
---
+++ components/card/section-cards.js
@@ -0,0 +1,18 @@
+import Card from "./card";
+import styles from "./section-cards.module.css";
+
+const SectionCards = (props) => {
+  const { title, videos, size } = props;
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.title}>{title}</h2>
+      <div className={styles.cardWrapper}>
+        {videos.map((video, idx) => {
+          return <Card key={idx} id={idx} imgUrl={video.imgUrl} size={size} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default SectionCards;
